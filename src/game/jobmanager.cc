ea84c9680017d23dc06fbd8c3505d5c001afb6e1
Jake Clarke <clarke.jake@gmail.com>
2015-12-03 23:06:44 +0000

    Replaced the job queue for a ring buffer and fmt'd

--- src/game/jobmanager.cc
+++ src/game/jobmanager.cc
@@ -1,4 +1,5 @@
 #include "jobmanager.h"
+#include "jobmanager.h"
 
 #include <iostream>
 #include <cassert>
@@ -7,210 +8,180 @@ using namespace phrix::game;
 
 int maxJobs = 1024;
 
-JobManager::JobManager(int maxThreads, int minThreads)
-    : maxThreads(maxThreads),
-      minThreads(minThreads),
-      suggestedNumThreads(minThreads),
-      started(false),
-      exiting(false),
-      pending(0),
-      processing(false),
-		queuedJobs(0) {}
-
-JobManager::JobManager(int maxThreads)
-    : maxThreads(maxThreads),
-      minThreads(std::thread::hardware_concurrency()),
-      suggestedNumThreads(minThreads),
-      started(false),
-      exiting(false),
-      pending(0),
-      processing(false),
-	  queuedJobs(0){}
+JobManager::JobManager(Game* game)
+	: m_game(game),
+	m_head(0),
+	m_tail(0),
+	minThreads(std::thread::hardware_concurrency()),
+	maxThreads(2 * minThreads),
+	suggestedNumThreads(minThreads),
+	started(false),
+	exiting(false),
+	pending(0),
+	processing(false) {}
 
 void JobManager::sched(std::unique_ptr<Job>& job) {
-  assert(job);
-  std::unique_lock<std::mutex> jl(jobQueueMutex);
-  if (queuedJobs < maxJobs)
-  {
-	  queuedJobs++;
-	  if (job->locked) {
-		  lockedJobQueue.emplace(std::move(job));
-	  }
-	  else {
-		  jobQueue.emplace(std::move(job));
-	  }
-  }
-  else {
-	  std::cerr << "too many jobs. dropping job." << std::endl;
-  }
+	assert(job);
+	std::unique_lock<std::mutex> jl(jobQueueMutex);
+	if (numJobs() < maxJobs)
+	{
+		m_jobs[m_head] = std::move(job);
+		m_head = (m_head + 1) % maxJobs;
+	}
+	else {
+		std::cerr << "too many jobs. dropping job." << std::endl;
+	}
 
 }
 
 void JobManager::start() {
-  if (!started) {
-    started = true;
-    activeThreads = 0;
-    suggestedNumThreads = minThreads;
-    workerthreads = std::unique_ptr<std::thread[]>(new std::thread[maxThreads]);
-    for (int i = 0; i < maxThreads; ++i) {
-      workerthreads[i] = createThread();
-    }
-  }
+	if (!started) {
+		started = true;
+		activeThreads = 0;
+		suggestedNumThreads = minThreads;
+		workerthreads = std::unique_ptr<std::thread[]>(new std::thread[maxThreads]);
+		for (int i = 0; i < maxThreads; ++i) {
+			workerthreads[i] = createThread();
+		}
+	}
+}
+
+size_t phrix::game::JobManager::numJobs()
+{
+	return m_head >= m_tail ? m_head - m_tail : (m_head + maxJobs) - m_tail;
 }
 
 void JobManager::sleepIfUnneeded() {
-  while (true) {
-    int active = activeThreads;
-    if (active > suggestedNumThreads) {
-      std::cout << active << "/" << suggestedNumThreads << std::endl;
-      if (activeThreads.compare_exchange_strong(active, active - 1)) {
-      	--pending;
-        std::unique_lock<std::mutex> l(workerSleep);
-        workerThreadSleepCv.wait(
-            l, [this]() { return activeThreads < suggestedNumThreads || exiting; });
-        ++activeThreads;
-        ++pending;
-      }
-    } else {
-      return;
-    }
-  }
+	while (true) {
+		int active = activeThreads;
+		if (active > suggestedNumThreads) {
+			std::cout << active << "/" << suggestedNumThreads << std::endl;
+			if (activeThreads.compare_exchange_strong(active, active - 1)) {
+				--pending;
+				std::unique_lock<std::mutex> l(workerSleep);
+				workerThreadSleepCv.wait(
+					l, [this]() { return activeThreads < suggestedNumThreads || exiting; });
+				++activeThreads;
+				++pending;
+			}
+		}
+		else {
+			return;
+		}
+	}
 }
 
 std::thread JobManager::createThread() {
-  ++activeThreads;
-  ++pending;
-  return std::thread([this]() {
-    while (!exiting) {
-      sleepIfUnneeded();
-      auto job = popJob();
-      assert(job || exiting);
-      if (job) {
-        job->awaitDependancies();
-        if (job->locked) {
-          job->run();
-        } else {
-          // we don't want to block the main thread for long jobs.
-          --pending;
-          job->run();
-          ++pending;
-        }
-        job->signalDependant();
-      }
-    }
-    --pending;
-    --activeThreads;
-  });
+	++activeThreads;
+	++pending;
+	return std::thread([this]() {
+		while (!exiting) {
+			sleepIfUnneeded();
+			auto job = popJob();
+			assert(job || exiting);
+			if (job) {
+				job->awaitDependancies();
+				if (job->locked) {
+					job->run();
+				}
+				else {
+					// we don't want to block the main thread for long jobs.
+					--pending;
+					job->run();
+					++pending;
+				}
+				job->signalDependant();
+			}
+		}
+		--pending;
+		--activeThreads;
+	});
 }
 
 void JobManager::wait() {
-  processing = true;
-  jobCv.notify_all();
-  while (true) {
-    std::unique_ptr<Job> job;
-    {
-      // main thread should only consume work that we can expect to complete in
-      // the current frame.
-      std::unique_lock<std::mutex> jl(jobQueueMutex);
-      assert(jl);
-      if (!lockedJobQueue.empty()) {
-        job = std::move(lockedJobQueue.front());
-        lockedJobQueue.pop();
-        assert(job);
-      } else {
-        break;
-      }
-    }
+	processing = true;
 
-    job->run();
-  }
+	std::unique_lock<std::mutex> jl(jobQueueMutex);
+	if (numJobs() > 0)
+	{
+		jobCv.notify_all();
+		mainThreadSleepCv.wait(jl, [this]() {
+			return numJobs() == 0 && pending == 0;
+		});
+	}
 
-  processing = false;
-
-  while (pending > 0) {
-    std::this_thread::yield();
-  }
+	processing = false;
 }
 
 std::unique_ptr<Job> JobManager::popJob() {
-  std::unique_ptr<Job> res;
-  while (true) {
-    std::unique_lock<std::mutex> jl(jobQueueMutex);
-    assert(jl);
-    // this is kind of messy buy i'd rather maintain the lock then reaquire.
-    if (!processing && !exiting) {
-      --pending;
-      jobCv.wait(jl, [this]() { return processing || exiting; });
-      ++pending;
-    }
+	std::unique_ptr<Job> res;
+	while (!res && !exiting) {
+		std::unique_lock<std::mutex> jl(jobQueueMutex);
+		assert(jl);
 
-    // if we woke up due to a exit signal don't process anything.
-    if (exiting) {
-      return res;
-    }
+		// Little bit messy doing this first but i don't want to reaquire the lock.
+		if (numJobs() == 0) {
+			--pending;
+			if (pending == 0)
+			{
+				mainThreadSleepCv.notify_one();
+			}
+			jobCv.wait(jl, [this]() { return (processing && numJobs() > 0) || exiting; });
+			++pending;
+		}
 
-    // todo a fairer selection from the queues.
-    if (!lockedJobQueue.empty()) {  // frame locked jobs second.
-      res = std::move(lockedJobQueue.front());
-      lockedJobQueue.pop();
-	  --queuedJobs;
-      return res;
-    } else if (!jobQueue.empty()) {  // long jobs next.
-      res = std::move(jobQueue.front());
-      jobQueue.pop();
-	  --queuedJobs;
-      return res;
-    } else {
-      std::this_thread::yield();
-    }
-  }
-  return res;
+		if (numJobs() > 0 && !exiting) {  // frame locked jobs second.
+			res = std::move(m_jobs[m_tail]);
+			m_tail = (m_tail + 1) % maxJobs;
+		}
+	}
+	return res;
 }
 
 void JobManager::notifyBlocked() {
-  if (suggestedNumThreads < maxThreads) {
-    ++suggestedNumThreads;
-    workerThreadSleepCv.notify_one();
-  }
+	if (suggestedNumThreads < maxThreads) {
+		++suggestedNumThreads;
+		workerThreadSleepCv.notify_one();
+	}
 }
 
 void JobManager::notifyUnblocked() {
-  if (suggestedNumThreads > minThreads) {
-    --suggestedNumThreads;
-  }
+	if (suggestedNumThreads > minThreads) {
+		--suggestedNumThreads;
+	}
 }
 
 JobManager::~JobManager() {
-  if (started) {
-    exiting = true;
-    jobCv.notify_all();
-    workerThreadSleepCv.notify_all();
-    for (int i = 0; i < maxThreads; ++i) {
-      workerthreads[i].join();
-    }
-  }
+	if (started) {
+		exiting = true;
+		jobCv.notify_all();
+		workerThreadSleepCv.notify_all();
+		for (int i = 0; i < maxThreads; ++i) {
+			workerthreads[i].join();
+		}
+	}
 }
 
 void Job::addDependancy(Job* j) {
-  this->remainingDependancies++;
-  j->dependancies.emplace_back(j);
+	this->remainingDependancies++;
+	j->dependancies.emplace_back(this);
 }
 
 void Job::signalDependant() {
-  for (auto j : dependancies) {
-    j->remainingDependancies--;
-    if (j->remainingDependancies == 0) {
-      j->dependanciesCV.notify_one();
-    }
-  }
+	for (auto j : dependancies) {
+		j->remainingDependancies--;
+		if (j->remainingDependancies == 0) {
+			j->dependanciesCV.notify_one();
+		}
+	}
 }
 
 void Job::awaitDependancies() {
-  if (remainingDependancies > 0) {
-    std::mutex dependanciesBlock;
-    std::unique_lock<std::mutex> l(dependanciesBlock);
-    parent->notifyBlocked();
-    dependanciesCV.wait(l, [this]() { return remainingDependancies == 0; });
-    parent->notifyUnblocked();
-  }
+	if (remainingDependancies > 0) {
+		std::mutex dependanciesBlock;
+		std::unique_lock<std::mutex> l(dependanciesBlock);
+		parent->notifyBlocked();
+		dependanciesCV.wait(l, [this]() { return this->remainingDependancies == 0; });
+		parent->notifyUnblocked();
+	}
 }
--- src/game/jobmanager.h
+++ src/game/jobmanager.h
@@ -5,86 +5,97 @@
 #include <thread>
 #include <memory>
 #include <mutex>
-#include <queue>
 #include <condition_variable>
 #include <vector>
 
 namespace phrix {
-namespace game {
+	namespace game {
+		class Game;
+		class JobManager;
 
-class JobManager;
+		class Job {
+			friend class JobManager;
+		public:
+			Job() : remainingDependancies(), locked(true) {
+				remainingDependancies = 0;
+			}
+			Job(bool locked) :remainingDependancies(0), locked(locked) {}
+			virtual ~Job() {}
+			void addDependancy(Job *);
+			JobManager * getParent() {
+				return parent;
+			}
+		protected:
 
-class Job {
-  friend class JobManager;
-public:
-	virtual ~Job() {}
-	void addDependancy(Job *);
-	JobManager * getParent() {
-		return parent;
+			virtual void run() = 0;
+
+		private:
+			std::atomic<int> remainingDependancies;
+			JobManager * parent;
+			void signalDependant();
+			void awaitDependancies();
+			bool locked;
+			std::vector<Job*> dependancies;
+
+
+			std::condition_variable dependanciesCV;
+		};
+
+		class JobManager {
+			friend class Job;
+		public:
+			JobManager(Game *game);
+			void sched(std::unique_ptr<Job> &);
+			void wait();
+			void start();
+			size_t numJobs();
+			Game *getGame() { return m_game; }
+			~JobManager();
+
+		private:
+			std::unique_ptr<Job> popJob();
+			void sleepIfUnneeded();
+			void notifyBlocked();
+			void notifyUnblocked();
+			std::thread createThread();
+
+			static constexpr size_t maxJobs = 256;
+
+			Game* m_game;
+			std::unique_ptr<Job> m_jobs[maxJobs];
+			size_t m_head;
+			size_t m_tail;
+
+			std::mutex jobQueueMutex;
+			/// cv to notify threads waiting on new jobs.
+			std::condition_variable jobCv;
+			/// mutex for threads that need to sleep to wait on.
+			std::mutex workerSleep;
+			/// cv to wait and notify sleeping threads.
+			std::condition_variable workerThreadSleepCv;
+
+			/// main thread cv
+			std::condition_variable mainThreadSleepCv;
+
+
+			/// min number of threads that this job manager runs at start.
+			/// hint: set to hw threads -1 .
+			int minThreads;
+
+			/// max number of threads that this job manager suppots.
+			int maxThreads;
+			/// suggested number of threads that should be running at any time.
+			std::atomic<int> suggestedNumThreads;
+			/// current number of threads that are seeking work.
+			std::atomic<int> activeThreads;
+
+			std::unique_ptr<std::thread[]> workerthreads;
+			std::atomic<int> pending;
+			bool started;
+			bool exiting;
+			std::atomic<bool> processing;
+		};
 	}
- protected:
-  Job() : locked(true) {}
-  Job(bool locked) : locked(locked) {}
-
-  virtual void run() = 0;
-  
- private:
- 	JobManager * parent;
- 	void signalDependant();
- 	void awaitDependancies();
-  bool locked;
-  std::vector<Job*> dependancies;
-  std::atomic<int> remainingDependancies;
-
-  std::condition_variable dependanciesCV;
-};
-
-class JobManager {
-	friend class Job;
- public:
-  JobManager(int max, int min);
-  JobManager(int max);
-  void sched(std::unique_ptr<Job> &);
-  void wait();
-  void start();
-  ~JobManager();
-
- private:
-  std::unique_ptr<Job> popJob();
-  void sleepIfUnneeded();
-  void notifyBlocked();
-  void notifyUnblocked();
-  std::thread createThread();
-
-  std::mutex jobQueueMutex;
-  std::queue<std::unique_ptr<Job>> jobQueue;
-  std::queue<std::unique_ptr<Job>> lockedJobQueue;
-  /// cv to notify threads waiting on new jobs.
-  std::condition_variable jobCv;
-  /// mutex for threads that need to sleep to wait on.
-  std::mutex workerSleep;
-  /// cv to wait and notify sleeping threads.
-  std::condition_variable workerThreadSleepCv;
-
-  /// max number of threads that this job manager suppots.
-  int maxThreads;
-  /// min number of threads that this job manager runs at start.
-  /// hint: set to hw threads -1 .
-  int minThreads;
-  /// suggested number of threads that should be running at any time.
-  std::atomic<int> suggestedNumThreads;
-  /// current number of threads that are seeking work.
-  std::atomic<int> activeThreads;
-
-  std::unique_ptr<std::thread[]> workerthreads;
-  std::atomic<int> pending;
-  bool started;
-  bool exiting;
-  std::atomic<bool> processing;
-
-  int queuedJobs;
-};
-}
 }
 
 #endif
